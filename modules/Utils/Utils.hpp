580a1b0e006b4db7191284b53c935778e0916fac
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2023-10-12 09:32:09 +0300

    add common utils, dot, euclidean, manhattan

--- modules/Utils/Utils.hpp
+++ modules/Utils/Utils.hpp
@@ -7,10 +7,18 @@
 #include <Data.hpp>
 #include <vector>
 
-void split_lsh_args(const int argc, const char *argv[], LshArgs &args);
+void splitLshArgs(const int argc, const char *argv[], LshArgs &args);
 
 void split_cube_args(const int argc, const char *argv[], CubeArgs &args);
 
 void split_cluster_args(const int argc, const char *argv[], ClusterArgs &args);
 
 void parseInputFile(const std::string &inputFile, Metadata &metadata, std::vector<std::vector<char>> &images);
+
+double DotProduct(const std::vector<double> &first, const std::vector<unsigned char> &second);
+
+int64_t Modulo(int64_t first, int64_t second);
+
+double EuclideanDistance(const std::vector<double> &first, const std::vector<unsigned char> &second);
+
+double ManhattanDistance(const std::vector<double> &first, const std::vector<unsigned char> &second);
