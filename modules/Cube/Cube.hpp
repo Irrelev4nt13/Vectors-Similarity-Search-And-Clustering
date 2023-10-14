1a8a570f5e22ed5ee8f08168cd0bfcb62b4aeaae
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-14 23:02:28 +0300

    cube cmd arguments

--- modules/Cube/Cube.hpp
+++ modules/Cube/Cube.hpp
@@ -2,33 +2,21 @@
 
 #include <iostream>
 
-class CubeArgs
-{
-public:
-    std::string input_file = "";
-    std::string query_file = "";
-    int dimension = 14;
-    int max_candidates = 10;
-    int probes = 2;
-    std::string output_file = "";
-    int num_of_nearest_neighbors = 1;
-    double radius = 10000;
-};
 class Cube
 {
 private:
     // The arguments initialized with the default values
-    std::string input_file;       // –d <input file>
-    std::string query_file;       // –q <query file>
-    int dimension;                // –k <int>
-    int max_candidates;           // -M <int>
-    int probes;                   // -probes <int>
-    std::string output_file;      // -ο <output file>
-    int num_of_nearest_neighbors; // -Ν <number of nearest>
-    double radius;                // -R <radius>
+    // std::string input_file;       // –d <input file>
+    // std::string query_file;       // –q <query file>
+    int dimension;     // –k <int>
+    int maxCanditates; // -M <int>
+    int probes;        // -probes <int>
+    // std::string output_file;      // -ο <output file>
+    int numNn;     // -Ν <number of nearest>
+    double radius; // -R <radius>
 
 public:
-    Cube(const CubeArgs &args);
+    Cube(int dimension, int maxCanditates, int probes, int numNn, double radius);
     ~Cube();
     void print_cube();
 };
