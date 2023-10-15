2ab3e4c7e73e8ebe8bffb6225cb2d3c9e7f09656
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2023-10-15 12:37:33 +0300

    add timers for tTrue and tLSH

--- src/lsh_main.cpp
+++ src/lsh_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <chrono>
 
 #include "Lsh.hpp"
 #include "LshCmdArgs.hpp"
@@ -51,8 +52,15 @@ int main(int argc, char const *argv[])
     {
         Image *query = query_images[i];
 
+        auto begin_lsh = std::chrono::high_resolution_clock::now();
         std::vector<std::tuple<Image *, double>> aprox_vector = lsh.Approximate_kNN(query);
+        auto end_lsh = std::chrono::high_resolution_clock::now();
+        auto elapsed_lsh = std::chrono::duration_cast<std::chrono::nanoseconds>(end_lsh - begin_lsh);
+
+        auto begin_brute = std::chrono::high_resolution_clock::now();
         std::vector<std::tuple<Image *, double>> brute_vector = BruteForce(input_images, query, args.numNn);
+        auto end_brute = std::chrono::high_resolution_clock::now();
+        auto elapsed_brute = std::chrono::duration_cast<std::chrono::nanoseconds>(end_brute - begin_brute);
 
         output_file << "Query: " << query->id << std::endl;
         // for (auto &tuple : aprox_vector)
@@ -66,8 +74,8 @@ int main(int argc, char const *argv[])
             dist = std::get<1>(brute_vector[i]);
             output_file << "distanceTrue: " << dist << "\n";
         }
-        output_file << "tLSH: " << std::endl;
-        output_file << "tTrue: " << std::endl;
+        output_file << "tLSH: " << elapsed_lsh.count() * 1e-9 << std::endl;
+        output_file << "tTrue: " << elapsed_brute.count() * 1e-9 << std::endl;
         // std::vector<Image *> range_vector = lsh.Approximate_Range_Search(query);
         output_file << "R-near neighbors:" << std::endl;
         // for (auto &image : range_vector)
