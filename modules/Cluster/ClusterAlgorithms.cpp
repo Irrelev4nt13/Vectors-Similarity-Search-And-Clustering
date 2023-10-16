ffa23355cd366756f3b33afd0caf17060ea0549a
Michail Chatzispyrou <mikexatzi.gr@gmail.com>
2023-10-16 17:20:58 +0300

    minor changes to cluster

--- modules/Cluster/ClusterAlgorithms.cpp
+++ modules/Cluster/ClusterAlgorithms.cpp
@@ -1,12 +1,24 @@
-
 #include <vector>
+#include <random>
 
+#include "Utils.hpp"
 #include "Image.hpp"
 #include "Cluster.hpp"
 #include "ClusterAlgorithms.hpp"
 
 std::vector<Cluster> KMeansPlusPlus(std::vector<Image *> input_images, int number_of_clusters)
 {
-    std::vector<Cluster> temp;
-    return temp;
+    std::uniform_int_distribution<> uniform_int(0, input_images.size());
+
+    int index = uniform_int(RandGen());
+
+    std::vector<Cluster> clusters;
+    clusters.push_back(Cluster(input_images[index]));
+    for (int t = 1; t < number_of_clusters; t++)
+    {
+        std::vector<double> distance;
+
+        std::vector<double> prob;
+    }
+    return clusters;
 }
