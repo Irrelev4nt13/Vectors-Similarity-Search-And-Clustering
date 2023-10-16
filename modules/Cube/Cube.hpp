4fbac3974cae31e09b51bb1580a90899f5f45402
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-16 22:35:41 +0300

    define fields and methods of cube

--- modules/Cube/Cube.hpp
+++ modules/Cube/Cube.hpp
@@ -2,6 +2,12 @@
 #define CUBE_HPP_
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#include "HashTable.hpp"
+#include "PublicTypes.hpp"
+#include "Image.hpp"
 
 class Cube
 {
@@ -11,11 +17,15 @@ private:
     int probes;        // -p probes
     int numNn;         // -Ν number of nearest Neighbors
     double radius;     // -R radius
+    std::vector<HashTable> hashTables;
+    std::vector<std::unordered_map<std::string, int>> cubeMaps;
 
 public:
     Cube(int dimension, int maxCanditates, int probes, int numNn, double radius);
     ~Cube();
     void print_cube();
+    std::vector<Neighbor> Approximate_kNN(ImagePtr query);
+    std::vector<ImagePtr> Approximate_Range_Search(ImagePtr query);
 };
 
 #endif
