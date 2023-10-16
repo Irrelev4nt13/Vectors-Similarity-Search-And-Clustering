3e71a410350ceb4e409a11ddd65f9f7957c98d09
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-16 23:42:20 +0300

    fix fields, constructor and destructor

--- modules/Cube/Cube.cpp
+++ modules/Cube/Cube.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-#include <Cube.hpp>
+#include <vector>
 
-Cube::Cube(int dimension, int maxCanditates, int probes, int numNn, double radius)
-    : dimension(dimension),
-      maxCanditates(maxCanditates),
-      probes(probes),
-      numNn(numNn),
-      radius(radius) {}
+#include "Cube.hpp"
+#include "HashTable.hpp"
 
-Cube::~Cube() {}
+Cube::Cube(const std::vector<ImagePtr> images, int w, int dimension, int maxCanditates, int probes, int numNn, double radius)
+    : dimension(dimension), maxCanditates(maxCanditates), probes(probes), numNn(numNn), radius(radius)
+{
+    this->hashTable = new HashTable(dimension, AmplifiedHashFunction(w, dimension, dimension));
+
+    for (int i = 0; i < images.size(); i++)
+    {
+        this->hashTable->insert(images[i]);
+    }
+}
+
+Cube::~Cube()
+{
+    delete this->hashTable;
+}
 
 void Cube::print_cube()
 {
--- modules/Cube/Cube.hpp
+++ modules/Cube/Cube.hpp
@@ -12,16 +12,16 @@
 class Cube
 {
 private:
-    int dimension;     // –k number of dimensions
-    int maxCanditates; // -M maximum candidates
-    int probes;        // -p probes
-    int numNn;         // -Ν number of nearest Neighbors
-    double radius;     // -R radius
-    std::vector<HashTable> hashTables;
-    std::vector<std::unordered_map<std::string, int>> cubeMaps;
+    int dimension;                 // –k number of dimensions
+    int maxCanditates;             // -M maximum candidates
+    int probes;                    // -p probes
+    int numNn;                     // -Ν number of nearest Neighbors
+    double radius;                 // -R radius
+    HashTable *hashTable;          // hypercube hash table
+    std::vector<CubeMap> cubeMaps; // map string hash values to {0, 1}^d
 
 public:
-    Cube(int dimension, int maxCanditates, int probes, int numNn, double radius);
+    Cube(const std::vector<ImagePtr> images, int w, int dimension, int maxCanditates, int probes, int numNn, double radius);
     ~Cube();
     void print_cube();
     std::vector<Neighbor> Approximate_kNN(ImagePtr query);
