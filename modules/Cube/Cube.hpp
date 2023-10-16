faad79377ef0db77aca4b8b55a7418583b928439
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-16 18:44:47 +0300

    cube add header guard

--- modules/Cube/Cube.hpp
+++ modules/Cube/Cube.hpp
@@ -1,22 +1,21 @@
-#pragma once
+#ifndef CUBE_HPP_
+#define CUBE_HPP_
 
 #include <iostream>
 
 class Cube
 {
 private:
-    // The arguments initialized with the default values
-    // std::string input_file;       // –d <input file>
-    // std::string query_file;       // –q <query file>
-    int dimension;     // –k <int>
-    int maxCanditates; // -M <int>
-    int probes;        // -probes <int>
-    // std::string output_file;      // -ο <output file>
-    int numNn;     // -Ν <number of nearest>
-    double radius; // -R <radius>
+    int dimension;     // –k number of dimensions
+    int maxCanditates; // -M maximum candidates
+    int probes;        // -p probes
+    int numNn;         // -Ν number of nearest Neighbors
+    double radius;     // -R radius
 
 public:
     Cube(int dimension, int maxCanditates, int probes, int numNn, double radius);
     ~Cube();
     void print_cube();
 };
+
+#endif
