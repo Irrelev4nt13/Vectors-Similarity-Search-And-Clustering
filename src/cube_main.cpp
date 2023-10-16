61d08e8fa2d7c2845fd707b9137c2c1439a1a0e1
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-10-17 00:03:45 +0300

    fixed errors

--- src/cube_main.cpp
+++ src/cube_main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
+#include <fstream>
 
 #include "CubeCmdArgs.hpp"
 #include "Cube.hpp"
+#include "FileParser.hpp"
 #include "Utils.hpp"
 
 int main(int argc, char const *argv[])
 {
     CubeCmdArgs args(argc, argv);
 
-    Cube cube(args.dimension, args.maxCanditates, args.probes, args.numNn, args.radius);
+    // Check if the input is empty in order to ask for it in the main
+    if (args.inputFile.empty())
+    {
+        std::cout << "Enter input file: ";
+        std::getline(std::cin, args.inputFile);
+    }
+    FileParser inputParser(args.inputFile);
+
+    const std::vector<ImagePtr> input_images = inputParser.GetImages();
+
+    // Check if the query is empty in order to ask for it in the main
+    if (args.queryFile.empty())
+    {
+        std::cout << "Enter query file: ";
+        std::getline(std::cin, args.queryFile);
+    }
+    FileParser queryParser(args.queryFile);
+
+    const std::vector<ImagePtr> query_images = queryParser.GetImages();
+
+    // std::fstream output_file()
+    if (args.outputFile.empty())
+    {
+        std::cout << "Enter output file: ";
+        std::getline(std::cin, args.outputFile);
+    }
+    std::ofstream output_file;
+    output_file.open(args.outputFile);
+
+    int w = 4;
+
+    Cube cube(input_images, w, args.dimension, args.maxCanditates, args.probes, args.numNn, args.radius);
 
     cube.print_cube();
 
