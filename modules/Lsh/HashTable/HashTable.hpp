8f32a2ed6c629e4fee46d3c38d5238aa6303a300
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2023-10-25 21:42:28 +0300

    comment hashtable

--- modules/Lsh/HashTable/HashTable.hpp
+++ modules/Lsh/HashTable/HashTable.hpp
@@ -8,6 +8,14 @@
 #include "HashFunction.hpp"
 #include "PublicTypes.hpp"
 
+/**
+ * @brief The class of a amplified lsh function (g_i) consists of the following
+ *
+ * @param r the random vector
+ * @param hash_functions all the hash_functions h_i that are used
+ *
+ * @method hash utilizies the h_i functions and a combination of the r vector to insert an image
+ */
 class AmpLsh
 {
 private:
@@ -21,6 +29,16 @@ public:
     int hash(ImagePtr image);
 };
 
+/**
+ * @brief The class of a HashTable consists of the following
+ *
+ * @param numBuckets the number of buckets which will be used which is essentially 2^k since {0,1}^d'
+ * @param buckets the buckets in a 2d vector form, the first vector is for the buckets and the second for the elements of each bucket
+ * @param hashmap the amplified hash function for the hashtable
+ *
+ * @method insert inserts an image into the buckets according to the hash
+ * @method get_bucket returns the bucket for the given image
+ */
 template <typename T>
 using Bucket = std::vector<T>;
 
