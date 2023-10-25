c91a2d94996c8000b9dd25194b32825a74aea81b
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2023-10-25 21:33:01 +0300

    comments for lsh

--- modules/Lsh/Lsh.hpp
+++ modules/Lsh/Lsh.hpp
@@ -3,10 +3,24 @@
 
 #include <vector>
 
-
 #include "Image.hpp"
 #include "HashTable.hpp"
 #include "ImageDistance.hpp"
+/**
+ * @brief The class of a cluster consists of the following
+ *
+ * @param numHashFuncs the dimension k = d' of hypercube
+ * @param numHtables the maximum number of points the algorithm will check
+ * @param probes the number of probes the algorithm will check (we check the initial bucket + probes which in reality is probes + 1)
+ * @param numNn the number of nearest neighbors needed
+ * @param w the window
+ * @param numBuckets the number of buckets which will be used
+ * @param hashtables this algorithm requires many hashtables, so we have a vector with objects HashTable which are essentially our own implementation to match our needs
+ * @param distance the generic distance
+ *
+ * @method Approximate_kNN returns a vector with numNn aproxximate nearest neighbors
+ * @method Approximate_Range_Search returns a vector with points inside the given radius
+ */
 class Lsh
 {
 private:
