cd8fdc68b60e210efe034db7060eafda905c2a4a
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2023-10-25 21:20:04 +0300

    comments for cube

--- modules/Cube/Cube.hpp
+++ modules/Cube/Cube.hpp
@@ -9,6 +9,25 @@
 #include "HashFunction.hpp"
 #include "ImageDistance.hpp"
 
+/**
+ * @brief The class of a cluster consists of the following
+ *
+ * @param dimension the dimension k = d' of hypercube
+ * @param maxCanditates the maximum number of points the algorithm will check
+ * @param probes the number of probes the algorithm will check (we check the initial bucket + probes which in reality is probes + 1)
+ * @param numNn the number of nearest neighbors needed
+ * @param w the window
+ * @param numBuckets the number of buckets which will be used which is essentially 2^k since {0,1}^d'
+ * @param buckets the buckets in a vector form
+ * @param map the map to match f_i(h_i()) values
+ * @param hash_functions the h_i functions that are used
+ * @param distance the generic distance
+ *
+ * @method hash utilizies the h_i functions to insert an image
+ * @method insert inserts an image into the buckets according to the hash
+ * @method Approximate_kNN returns a vector with numNn aproxximate nearest neighbors
+ * @method Approximate_Range_Search returns a vector with points inside the given radius
+ */
 class Cube
 {
 private:
